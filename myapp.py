41695382da58caac4d341291fdcd3d87bf878e99
Amikoell <zidanamikulafham674@gmail.com>
2024-10-01 23:31:21 +0700

    clone from laptop

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return'<h1 style=" color:green;" > Hello Girl...<\h1>'
+    return'<h1 style=" color:green;" > Hello Girl... cogan here...'
 
 
 if __name__ == "__main__":
